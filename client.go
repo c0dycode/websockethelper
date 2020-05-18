ad0297dcc831759fc75c3bff217b22186f130761
c0dycode <salx92@gmail.com>
2020-05-18 17:52:32 +0200

    Added files

---
+++ client.go
@@ -0,0 +1,108 @@
+package websockethelper
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	// Constant for the amount of items a channel can hold before blocking
+	channelSize = 100
+)
+
+// SocketClient is the struct that stores the webSocket Connection
+type SocketClient struct {
+	sendChannel chan SocketMessage
+	readChannel chan SocketMessage
+	callbacks   map[string][]func(SocketMessage)
+	hub         *WebSocketHub
+	conn        *websocket.Conn
+}
+
+// SocketMessage is the struct that we send and receive from the javascript side of the webui
+type SocketMessage struct {
+	EventName string `json:"eventName"`
+	Content   string `json:"content"`
+	Error     string `json:"error"`
+}
+
+// ServeWS is the handler for requests to connect via websocket
+func ServeWS(hub *WebSocketHub, w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r, nil, 2048, 2048)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := &SocketClient{
+		sendChannel: make(chan SocketMessage, channelSize),
+		readChannel: make(chan SocketMessage, channelSize),
+		callbacks:   make(map[string][]func(SocketMessage)),
+		hub:         hub,
+		conn:        ws,
+	}
+	for {
+		if len(logBuffer) > 0 {
+			if msg, ok := <-logBuffer; ok {
+				client.sendChannel <- msg
+			}
+		} else {
+			break
+		}
+	}
+	client.hub.register <- client
+	go client.readPump()
+	go client.writePump()
+}
+
+func (s *SocketClient) writePump() {
+	defer func() {
+		s.hub.unregister <- s
+		s.conn.Close()
+	}()
+	for {
+		select {
+		case msg, ok := <-s.sendChannel:
+			if len(msg.Content) > 0 && ok {
+				err := s.conn.WriteJSON(msg)
+				if err != nil {
+					fmt.Printf("error while writing json: %v\n", err)
+					return
+				}
+				break
+			}
+		default:
+			break
+		}
+		time.Sleep(time.Millisecond * 50)
+	}
+}
+
+func (s *SocketClient) readPump() {
+	defer func() {
+		s.hub.unregister <- s
+		s.conn.Close()
+	}()
+	for {
+		_, b, err := s.conn.ReadMessage()
+		if err == nil {
+			var msg SocketMessage
+			err = json.Unmarshal(b, &msg)
+			if err == nil && len(s.readChannel) < channelSize {
+				sm.RLock()
+				defer sm.RUnlock()
+				if action, ok := s.callbacks[msg.EventName]; ok {
+					for _, ac := range action {
+						ac(msg)
+					}
+				}
+			}
+		} else {
+			return
+		}
+		time.Sleep(time.Millisecond * 50)
+	}
+}
---
+++ hub.go
@@ -0,0 +1,138 @@
+package websockethelper
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// LogType determines the color of the webui log-output for that specific item
+type LogType int
+
+const (
+	logBufferSize = 250
+
+	// LogOk green message in the webui log
+	LogOk LogType = 0
+	// LogInfo yellow message in the webui log
+	LogInfo LogType = 1
+	// LogError red message in the webui log
+	LogError LogType = 2
+)
+
+var (
+	hub       *WebSocketHub
+	logBuffer chan SocketMessage
+	callbacks map[string][]func(SocketMessage)
+	sm        sync.RWMutex
+)
+
+// WebSocketHub keeps track of the connections and registers and unregisters them
+type WebSocketHub struct {
+	clients    map[*SocketClient]bool
+	register   chan *SocketClient
+	unregister chan *SocketClient
+	broadcast  chan SocketMessage
+}
+
+func init() {
+	logBuffer = make(chan SocketMessage, logBufferSize)
+	callbacks = make(map[string][]func(SocketMessage))
+}
+
+// NewHub returns a new WebSocketHub
+func NewHub() *WebSocketHub {
+	hub = &WebSocketHub{
+		clients:    make(map[*SocketClient]bool),
+		register:   make(chan *SocketClient),
+		unregister: make(chan *SocketClient),
+		broadcast:  make(chan SocketMessage, 250),
+	}
+	return hub
+}
+
+// Run starts the WebSocketHub and manages connections
+func (wh *WebSocketHub) Run() {
+	for {
+		select {
+		case client := <-wh.register:
+			wh.clients[client] = true
+			sm.Lock()
+			client.callbacks = callbacks
+			sm.Unlock()
+		case client := <-wh.unregister:
+			if _, ok := wh.clients[client]; ok {
+				delete(wh.clients, client)
+				close(client.sendChannel)
+				close(client.readChannel)
+			}
+		case msg := <-wh.broadcast:
+			if len(wh.clients) == 0 {
+				logBuffer <- msg
+			}
+			for client := range wh.clients {
+				select {
+				case client.sendChannel <- msg:
+				default:
+					close(client.sendChannel)
+					delete(wh.clients, client)
+				}
+			}
+		}
+	}
+}
+
+func broadcastMessages(msgToSend SocketMessage) {
+	hub.broadcast <- msgToSend
+}
+
+// RegisterCallback allows you to register functions outside of this package to be called on specific eventnames
+func RegisterCallback(eventName string, f func(SocketMessage)) {
+	sm.Lock()
+	defer sm.Unlock()
+	for client := range hub.clients {
+		var found bool = false
+		if ac, _ := client.callbacks[eventName]; ac != nil {
+			for _, a := range ac {
+				if &a == &f {
+					found = true
+				}
+			}
+		}
+		if !found {
+			client.callbacks[eventName] = append(client.callbacks[eventName], f)
+		}
+	}
+	callbacks[eventName] = append(callbacks[eventName], f)
+}
+
+// SendMessageToWS adds the message formatted to the SendChannel
+func SendMessageToWS(message SocketMessage) error {
+	broadcastMessages(message)
+	return nil
+}
+
+// SendLogMessageToWS adds the message formatted to the SendChannel
+func SendLogMessageToWS(message string, errorType LogType) {
+	t := time.Now()
+	y, mon, d := t.Date()
+	h, m, sec := t.Clock()
+	formattedMessage := fmt.Sprintf("%d-%d-%d %02d:%02d:%02d : %s", y, mon, d, h, m, sec, message)
+
+	var msg SocketMessage
+	msg.Content = formattedMessage
+	msg.EventName = "LogMessage"
+
+	switch errorType {
+	case LogOk:
+		msg.Error = "ok"
+		break
+	case LogInfo:
+		msg.Error = "info"
+		break
+	case LogError:
+		msg.Error = "error"
+		break
+	}
+	broadcastMessages(msg)
+}
